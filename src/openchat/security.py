eb686f587ee864501bd38391c18050756ef2e7c5
root <root@cloud.(none)>
2014-10-05 00:16:21 +0800

    debug

--- src/openchat/security.py
+++ src/openchat/security.py
@@ -1,23 +1,20 @@
 import hashlib
 
 import openchat
-import openchat.config
 
 def verify(args):
-	try:
-		signature = args["signature"]
-		timestamp = args["timestamp"]
-		nonce = args["nonce"]
+	signature = args["signature"]
+	timestamp = args["timestamp"]
+	nonce = args["nonce"]
 
-		token = openchat.config.core.verify.token.get()
-		tmpArr = [token, timestamp, nonce]
-		tmpArr.sort()
-		tmpStr = "".join(tmpArr)
-		tmpStr = hashlib.sha1(tmpStr).hexdigest()
-		if tmpStr == signature:
-			return str(args["echostr"])
-		else:
-			return str(None)
-	except Exception:
-		return "Error"
+	token = openchat.config.setting.core.verify.token.get()
+	tmpArr = [token, timestamp, nonce]
+	tmpArr.sort()
+	tmpArr = tuple(tmpArr)
+	tmpStr = "%s%s%s" % tmpArr
+	tmpStr = hashlib.sha1(tmpStr).hexdigest()
+	if tmpStr == signature:
+		return str(args["echostr"])
+	else:
+		return "bad man!!!"
 
